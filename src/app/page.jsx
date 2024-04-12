a426e569cd2eb9ba6ec14a951cfea865effeeb96
prathamdupare <prathamdupare@proton.me>
2024-04-13 02:29:02 +0530

    fixed text size

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,17 +14,17 @@ const Homepage = () => {
     >
       <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* IMAGE CONTAINER */}
-        <div className="h-1/2 lg:h-full lg:w-1/2 m-3 relative">
+        <div className="h-1/2 lg:h-full lg:w-1/2 relative">
           <Image src="/hero.png" alt="" fill className="object-contain" />
         </div>
         {/* TEXT CONTAINER */}
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
           {/* TITLE */}
-          <h1 className="text-4xl md:text-6xl font-bold">
+          <h1 className="text-md md:text-6xl font-bold">
             Crafting Digital Experiences, Designing Tomorrow.
           </h1>
           {/* DESC */}
-          <p className="md:text-xl">
+          <p className="text-sm md:text-xl">
             Welcome to my portfolio, a fusion of innovation and creativity.
             Possessing a discerning eye for aesthetics and a mastery of code, my
             collection of projects reflects a diverse range that underscores my
